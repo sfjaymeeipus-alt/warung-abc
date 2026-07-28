10ab17f66506347794e61177a7b1d68a996eaab0
puspitadewi <sfjaymeeipus@gmail.com>
2026-07-28 12:22:38 +0700

    belum beres tinggal proses logout satu lagi

---
+++ buat_awal.php
@@ -0,0 +1,17 @@
+<?php
+include 'config/koneksi.php';
+
+$nama = 'Administrator';
+$username = 'admin';
+$password = password_hash('admin123', PASSWORD_DEFAULT);
+$role = 'admin';
+
+$sql = "INSERT INTO tbl_user (nama_lengkap, username, password, role)";
+$sql = "VALUES ('$nama', '$username', '$password', '$role')";
+
+if (mysqli_query($koneksi, $sql)) {
+    echo 'User admin berhasil dibuat. Silahkan hapus file ini.';
+} else {
+    echo 'Gagal membuat user: ' . mysqli_error($koneksi);
+}
+?>
